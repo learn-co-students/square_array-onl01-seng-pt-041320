b65b129e2d6014f7d56f88f75b539f23eb8636a9
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-13 22:54:07 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  # Creat a new array to store the squared values
+  new_sqr_arr = []
+  
+  # Square the values of the array and store them
+  array.each{|num| new_sqr_arr << num ** 2}
+  
+  # Return the new array of squared values
+  new_sqr_arr
+    
 end
