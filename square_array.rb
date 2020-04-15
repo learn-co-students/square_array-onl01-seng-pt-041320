21e74490635fe02a3eed6fb8a1c7928a248668a9
Patricia Cordero <patricia@patriciaco.com>
2020-04-15 01:25:47 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,14 @@
+# def square_array(array)
+#   square = []
+#   array.each do |num| square << num ** 2
+#   # square
+#   end
+#   square
+# end
+
+
 def square_array(array)
   square = []
-  array.each do |num| square << num ** 2
-  # square
-  end
+  array.each { |num| square << num ** 2 }
   square
 end
-
