f8f638d625338a98c19bb042116bd67c63168bf9
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-18 21:46:53 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+new_array=[]
+  array.each do |num|
+    new_array.push(num*num)
+
+  end
+  new_array
 end
+
+# numbers = [1, 2, 3]
+# numbers.each
+# square_array(numbers)
