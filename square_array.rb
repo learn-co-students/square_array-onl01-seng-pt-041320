c80c90d830e6c82cd280ab0026e5d986c0d734a3
vedransehovic <vedran.sehovic@gmail.com>
2020-04-18 21:04:34 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,6 @@
 numbers = [1, 2, 3, 4] #defining an array
-def square_array (array)
+
+def square_array (array) # method that takes an array of numbers, squares every member of that array and stores it into a new array. It returns that numbers_squared array
    numbers_squared = []
   array.each do |number|
    numbers_squared << number ** 2
