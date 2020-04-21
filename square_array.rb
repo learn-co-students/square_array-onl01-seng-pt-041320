93713eae15d63cff1b2701fb1d8e6d955f25fad6
Aurora Avery <aurora.avery@yahoo.com>
2020-04-21 12:44:52 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 end
+
+# Advanced level interator .collect
+# def square_array(array)
+#   array.collect {|element| element ** 2}
+# end 
